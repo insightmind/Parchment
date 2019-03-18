b1206fef8a52dab0c7f42db5a4612b62ba3d2c4b
Niklas Bülow <github@insightmind.net>
2019-03-18 15:07:13 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "Parchment",
+    products: [
+        .library(name: "Parchment", targets: ["Parchment"])
+    ],
+    targets: [
+        .target(
+            name: "Parchment",
+            path: "Parchment",
+        )
+    ]
+)
